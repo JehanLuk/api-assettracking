9c65c1b18706479813d0aa54932bb6d10499661d
Jean <lo856415@gmail.com>
2025-09-02 22:28:24 -0300

    feat(api): DTO configuration addded

--- AssetTrackingAPI/DTO/AssetDTO.cs
+++ AssetTrackingAPI/DTO/AssetDTO.cs
@@ -1,1 +1,14 @@
 namespace AssetTrackingAPI.DTO;
+
+public class AssetDTO
+{
+    [Required(ErrorMessage = "The asset must have a name!")]
+    [StringLength(255)]
+    public string Name { get; set; } = default!;
+
+    [Required(ErrorMessage = "The description is required!")]
+    [Column(TypeName = "text")]
+    public string Description { get; set; }
+
+    public AssetEnumStatus Status { get; set; } = AssetEnumStatus.Active;
+}
